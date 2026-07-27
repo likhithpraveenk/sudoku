d76a971b69b506c929916c44c5227a0aadb39aba
likhithpraveenk <lpkatlagunta@gmail.com>
2026-07-27 20:27:20 +0530

    fix ci & android build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,4 +1,3 @@
-import com.android.build.gradle.internal.api.ApkVariantOutputImpl
 import java.util.Properties
 import java.io.FileInputStream
 
@@ -69,6 +68,18 @@ android {
     dependenciesInfo {
         includeInApk = false
         includeInBundle = false
+    }
+    val abiCodes = mapOf("armeabi-v7a" to 1, "arm64-v8a" to 2, "x86_64" to 3)
+    applicationVariants.configureEach {
+        val variant = this
+        variant.outputs.forEach { output ->
+            val abiVersionCode =
+                abiCodes[output.filters.find { it.filterType == "ABI" }?.identifier]
+            if (abiVersionCode != null) {
+                (output as com.android.build.gradle.internal.api.ApkVariantOutputImpl).versionCodeOverride =
+                    variant.versionCode * 10 + abiVersionCode
+            }
+        }
     }
 }
 
--- android/app/proguard-rules.pro
+++ android/app/proguard-rules.pro
@@ -1,8 +1,2 @@
 # For more details, see
 #   http://developer.android.com/guide/developing/tools/proguard.html
-
-# Suppress Play Core warnings
--dontwarn com.google.android.play.core.**
-
-# Flutter
--keep class io.flutter.** { *; }
